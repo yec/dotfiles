cc17cea2bc0f040b5949a5caf0fe99abc635c347
Ye Chuah <yechuah@gmail.com>
2012-12-23 23:14:02 +1100

    use techlivezheng minibufexpl. and use git reset instead of git pull

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ else
 fi
 
 git submodule update --init --recursive
-git submodule foreach --recursive git pull origin master
+git submodule foreach --recursive git checkout master;git fetch origin;git reset --hard origin/master
 
 # setup command-t
 # cd _vim/bundle/command-t
