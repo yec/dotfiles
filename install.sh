4ac2cd281dd44db7515b56417f338413c511b87e
Ye Chuah <yechuah@gmail.com>
2013-01-23 21:17:15 +1100

    Don't run git submodule stuff in install file

--- install.sh
+++ install.sh
@@ -37,6 +37,3 @@ else
         link_file $i
     done
 fi
-
-git submodule update --init --recursive
-git submodule foreach --recursive git clean -fd; git fetch origin; git reset --hard origin/master
